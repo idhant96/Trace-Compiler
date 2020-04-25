635f6a86885c60d2017105b9f5029600ba5980cc
Rahul Suresh <rsures13@asu.edu>
2020-04-24 21:05:50 -0700

    Adding evaluation for the following predicates: 1. Block 2. Declaration 3. Statement 	i. Print Statement 	ii. Loop Statement 	     1. For Loop 	     2. Range For Statement              3. While Statement 	iii. Conditional Statement 	     1. If statement 	     2. If then Else then 	     3. If then Else if then

--- src/project.pl
+++ src/project.pl
@@ -1,3 +1,142 @@
+:- table number_expr/3, level_1/3, level_2/3, bool_expr/3, string_expr/3.
+%:- use_rendering(svgtree).
+
+program(X)  --> [execute], block(X).
+
+eval_program(P) :- eval_block(P, [], _, 0). 
+
+block(tree_block(X,Y)) --> ['{'], declarationList(X), statementList(Y), ['}'].
+eval_block(tree_block(X,Y), Env, EnvR, Scope) :- Scope1 is Scope + 1, eval_declarationList(X, Env, Env1, Scope1), eval_statementList(Y, Env1, Env2, Scope1), deleteScopeVariables(Env2,Scope1, EnvR).  
+
+/* Declaration part */
+ %------  
+declarationList(tree_declList(X,Y)) --> declaration(X), [';'], declarationList(Y).
+declarationList(tree_declList(empty)) --> [].
+
+eval_declarationList(tree_declList(X,Y), Env, EnvR, Scope) :- eval_declaration(X, Env, Env1, Scope), eval_declarationList(Y, Env1, EnvR, Scope).
+eval_declarationList(tree_declList(empty), Env, Env, _). 
+ %------  
+declaration(tree_decl_number(X)) --> numberDeclaration(X).
+declaration(tree_decl_boolean(X)) --> booleanDeclaration(X). 
+declaration(tree_decl_string(X)) --> stringDeclaration(X).
+
+
+eval_declaration(tree_decl_number(X),Env,EnvR,Scope):- eval_numberDeclaration(X,Env,EnvR,Scope).
+eval_declaration(tree_decl_boolean(X),Env,EnvR,Scope):- eval_booleanDeclaration(X,Env,EnvR,Scope).
+eval_declaration(tree_decl_string(X),Env,EnvR,Scope):- eval_stringDeclaration(X,Env,EnvR,Scope).
+ %------  
+numberDeclaration(tree_numDecl(X,Y)) --> [number], var_name(X), ['='], number_expr(Y).
+numberDeclaration(tree_numDecl(X)) --> [number], var_name(X).
+
+eval_numberDeclaration(tree_numDecl(X,Y),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)), eval_numberExp(Y,Env,Scope, Val), update(X,Val,Scope,Env,EnvR).
+eval_numberDeclaration(tree_numDecl(X,_),Env,Env,Scope):- lookup(X,Env,Scope,_), string_concat(X, " : Variable already declared", M), writeln(M), false. 
+
+eval_numberDeclaration(tree_numDecl(X),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)), update(X,0,Scope,Env,EnvR).
+eval_numberDeclaration(tree_numDecl(X),Env,Env,Scope):- lookup(X,Env,Scope,_), write(X),writeln(" : Variable already declared"), false. 
+    
+ %------   
+booleanDeclaration(tree_boolDecl(X,Y)) --> [bool], var_name(X), ['='], bool_expr(Y).
+booleanDeclaration(tree_boolDecl(X)) --> [bool], var_name(X).
+
+
+eval_booleanDeclaration(tree_boolDecl(X,Y),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)),eval_boolExp(Y,Val, Env,Scope), update(X,Val,Scope,Env, EnvR).
+eval_booleanDeclaration(tree_boolDecl(X,_),Env,Env,Scope):- lookup(X,Env,Scope,_), write(X), writeln(" : Variable already declared"), false. 
+
+eval_booleanDeclaration(tree_boolDecl(X),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)), update(X,false,Scope,Env,EnvR).
+eval_booleanDeclaration(tree_boolDecl(X),Env,Env,Scope):- lookup(X,Env,Scope,_), write(X), writeln(" : Variable already declared"), false. 
+    
+%------    
+stringDeclaration(tree_stringDecl(X,Y)) --> [string], var_name(X), ['='], string_expr(Y).
+stringDeclaration(tree_stringDecl(X)) --> [string], var_name(X).
+
+eval_stringDeclaration(tree_stringDecl(X,Y),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)),eval_stringExp(Y, Env,Scope, Val), update(X,Val,Scope,Env,EnvR).
+eval_stringDeclaration(tree_stringDecl(X,_),Env,Env,Scope):- lookup(X,Env,Scope,_), writeln(" : Variable already declared"), false. 
+
+eval_stringDeclaration(tree_stringDecl(X),Env,EnvR,Scope):- not(lookup(X,Env,Scope,_)), update(X,"",Scope,Env,EnvR).
+eval_stringDeclaration(tree_stringDecl(X),Env,Env,Scope):- lookup(X,Env,Scope,_), write(X), writeln(" : Variable already declared"), false. 
+    
+
+%-----
+
+/* Statements part */
+statementList(tree_statementList(X,Y)) --> statement(X), [';'], statementList(Y).
+statementList(tree_statementList(X,Y)) --> loopStatement(X), statementList(Y).
+statementList(tree_statementList(X,Y)) --> conditionalStatement(X), statementList(Y).
+statementList(tree_statementList(empty)) --> [].
+
+
+eval_statementList(tree_statementList(X,Y), Env,EnvR,Scope):-  eval_statement(X,Env,Env1,Scope), eval_statementList(Y,Env1,EnvR,Scope).
+eval_statementList(tree_statementList(X,Y),Env,EnvR,Scope):-  eval_loopStatement(X,Env,Env1,Scope), eval_statementList(Y,Env1,EnvR,Scope).
+eval_statementList(tree_statementList(X,Y),Env,EnvR,Scope):-  eval_conditionalStatement(X,Env,Env1,Scope), eval_statementList(Y,Env1,EnvR,Scope).
+eval_statementList(tree_statementList(empty),Env,Env,_).
+
+%-----
+statement(tree_print_statement(X)) --> print_statement(X).
+statement(tree_statement_number(X,Y)) --> var_name(X), ['='], number_expr(Y).
+statement(tree_statement_bool(X,Y)) --> var_name(X), ['='], bool_expr(Y).
+statement(tree_statement_string(X,Y)) --> var_name(X), ['='], string_expr(Y).
+statement(tree_statement_increment(X)) --> var_name(X), ['+'], ['+'].
+statement(tree_statement_decrement(X)) --> var_name(X), ['-'], ['-'].
+
+eval_statement(tree_print_statement(X), Env,Env,Scope) :- eval_print_statement(X,Env,Scope).
+eval_statement(tree_statement_number(X,Y), Env,EnvR,Scope) :-  lookup_for_previous_scope(X,Env,Scope,_), eval_numberExp(Y,Env,Scope,Val), find_scope(X,Env,Scope,ActualScope), update(X,Val,ActualScope,Env,EnvR).
+eval_statement(tree_statement_number(X,_), Env,_,Scope) :-  not(lookup_for_previous_scope(X,Env,Scope,_)), write(X), writeln(" : Variable is not declared"), false.
+
+eval_statement(tree_statement_bool(X,Y), Env,EnvR,Scope) :-  lookup_for_previous_scope(X,Env,Scope,_), eval_boolExp(Y,Val,Env,Scope), find_scope(X,Env,Scope,ActualScope), update(X,Val,ActualScope,Env,EnvR).
+eval_statement(tree_statement_bool(X,_), Env,_,Scope) :-  not(lookup_for_previous_scope(X,Env,Scope,_)), write(X), writeln(" : Variable is not declared"), false.
+
+eval_statement(tree_statement_string(X,Y), Env,EnvR,Scope) :- lookup_for_previous_scope(X,Env,Scope,_), find_scope(X,Env,Scope,ActualScope), update(X,Y,ActualScope,Env,EnvR).
+
+eval_statement(tree_statement_increment(X), Env,EnvR,Scope) :- lookup_for_previous_scope(X,Env,Scope,Val), Add is Val + 1, find_scope(X,Env,Scope,ActualScope), update(X,Add,ActualScope,Env,EnvR).
+eval_statement(tree_statement_decrement(X), Env,EnvR,Scope) :- lookup_for_previous_scope(X,Env,Scope,Val), Sub is Val - 1, find_scope(X,Env,Scope,ActualScope), update(X,Sub,ActualScope,Env,EnvR).
+
+%---
+
+print_statement(tree_print(X)) --> [print],['('], string_expr(X), [')'].
+print_statement(tree_println(X)) --> [println],['('], string_expr(X), [')'].
+
+eval_print_statement(tree_print(X), Env, Scope) :- eval_stringExp(X, Env, Scope, Val), write(Val).
+eval_print_statement(tree_println(X), Env, Scope) :- eval_stringExp(X, Env, Scope, Val), writeln(Val).
+
+%---
+
+/* Looping Statements -> (for loop, for in range() loop and while loop ) */
+loopStatement(tree_simpleFOR(X,Y,Z,A)) --> [for], ['('], declaration(X), [';'], bool_expr(Y) , [';'], statement(Z), [')'], block(A).
+loopStatement(tree_rangeFOR(X,Y,Z,A)) --> [for], var_name(X), [in], [range], ['('], number(Y), [','], number(Z) ,[')'], block(A).
+loopStatement(tree_while(X,Y)) --> [while],['('], bool_expr(X) ,[')'], block(Y).
+
+
+eval_loopStatement(tree_simpleFOR(X,Y,Z,A), Env, EnvR, Scope) :- Scope1 is Scope + 1, eval_declaration(X, Env, Env1, Scope1), 
+    eval_loopWithStatement(Y, Z, A, Env1, Env2, Scope1), deleteScopeVariables(Env2,Scope1,EnvR).
+
+eval_loopStatement(tree_rangeFOR(X,Y,Z,A), Env, EnvR, Scope) :- eval_loopStatement(tree_simpleFOR(tree_decl_number(tree_numDecl(X,tree_num(Y))),
+                                                                                                  tree_lesser(tree_variable(X),tree_num(Z)), 
+                                                                                                  tree_statement_increment(X), A), Env, EnvR, Scope). 
+
+eval_loopStatement(tree_while(X,Y),Env,EnvR,Scope):- eval_boolExp(X,Env,Scope,Val),Val = true, eval_block(Y, Env, Env1, Scope),eval_loopStatement(tree_while(X,Y),Env1,EnvR,Scope).
+eval_loopStatement(tree_while(X,_),EnvR,EnvR,Scope):- eval_boolExp(X,EnvR,Scope,Val),Val = false.
+
+eval_loopWithStatement(Y, Z, A, Env, EnvR, Scope) :- eval_boolExp(Y, Env, Scope, Val), Val = true, eval_block(A, Env, Env1, Scope), eval_statement(Z, Env1, Env2, Scope),
+    eval_loopWithStatement(Y, Z, A, Env2, EnvR, Scope).
+eval_loopWithStatement(Y, _, _, Env, Env, Scope) :- eval_boolExp(Y, Env, Scope, Val), Val = false.
+
+
+/* Conditional Statements -> (empty, if then, if then else, if then else if... ) */
+conditionalStatement(tree_IF(X,Y)) --> [if], ['('], bool_expr(X) , [')'], [then], block(Y).
+conditionalStatement(tree_if_then_else(X,Y,Z)) --> [if], ['('], bool_expr(X) , [')'], [then], block(Y) , [else], [then], block(Z).
+conditionalStatement(tree_if_then_else_if(X,Y,Z)) --> [if], ['('], bool_expr(X) , [')'], [then], block(Y) , [else], conditionalStatement(Z).
+conditionalStatement(tree_ternary(X,Y,Z)) --> bool_expr(X), ['?'], statement(Y), [':'], statement(Z). 
+                        
+eval_conditionalStatement(tree_IF(X,Y), Env, EnvR, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = true, eval_block(Y, Env, EnvR, Scope).
+eval_conditionalStatement(tree_IF(X,_), Env, Env, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = false.
+eval_conditionalStatement(tree_if_then_else(X,Y,_), Env, EnvR, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = true, eval_block(Y, Env, EnvR, Scope).
+eval_conditionalStatement(tree_if_then_else(X,_,Z), Env, EnvR, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = false, eval_block(Z, Env, EnvR, Scope).
+eval_conditionalStatement(tree_if_then_else_if(X,Y,_), Env, EnvR, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = true, eval_block(Y, Env, EnvR, Scope).
+eval_conditionalStatement(tree_if_then_else_if(X,_,Z), Env, EnvR, Scope) :- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = false, eval_conditionalStatement(Z, Env, EnvR, Scope).
+eval_conditionalStatement(tree_ternary(X,Y,_),Env,EnvR,Scope):- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = true,  eval_statement(Y,Env,EnvR,Scope).
+eval_conditionalStatement(tree_ternary(X,_,Z),Env,EnvR,Scope):- eval_boolExp(X,Env,Scope,BoolVal), BoolVal = false,  eval_statement(Z,Env,EnvR,Scope).
+
+
 number_expr(tree_strlen(X))--> [len],['('],string_expr(X),[')'].
 number_expr(tree_add(X,Y)) --> number_expr(X), ['+'], level_1(Y).
 number_expr(tree_subtract(X,Y)) --> number_expr(X), ['-'], level_1(Y).
@@ -21,7 +160,6 @@ eval_level1(tree_multiplication(X,Y), Env, Scope, Val) :- eval_numberExp(X, Env,
 eval_level1(tree_division(X,Y), Env, Scope, Val) :- eval_numberExp(X, Env, Scope, Val1), eval_level2(Y, Env, Scope, Val2), Val is Val1 / Val2.
 eval_level1(tree_mod(X,Y), Env, Scope, Val) :- eval_numberExp(X, Env, Scope, Val1), eval_level2(Y, Env, Scope, Val2), Val is mod(Val1, Val2).
 eval_level1(X, Env, Scope, Val) :- eval_level2(X, Env, Scope, Val).
-
 eval_level2(tree_braces(X), Env, Scope, Val) :- eval_numberExp(X, Env, Scope, Val).
 eval_level2(tree_num(X), _, _, X).
 eval_level2(tree_variable(X), Env, Scope, Val) :- eval_variable(X, Env, Scope, Val).
