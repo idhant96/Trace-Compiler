93588b8b70db1ece7f1e2ecdb44564567d9bf1a3
idhant96 <idhant123@gmail.com>
2020-04-28 21:46:43 -0700

    linux setup

--- linux_setup.sh
+++ linux_setup.sh
@@ -19,11 +19,24 @@ wget \
     && bash Miniconda3-latest-Linux-x86_64.sh -b \
     && rm -f Miniconda3-latest-Linux-x86_64.sh \
 conda --version
-source ~/.bashrc
+
+pip install -r requirements.txt
+# source ~/.bashrc
 # create new prolog environment
-conda env create -f environment.yml
+# conda env create -f environment.yml
+
+if [ $major -lt  3 ]
+then
+	echo "Intall python version greater than 3.5"
+else
+	minor=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+	if [ $minor -lt 5 ]
+	then
+		echo "Install python version greater than 3."
+	fi
+fi
 
 # ACTIVATE THE NEW ENVIRONMENT
-conda activate prolog
+# conda activate prolog
 
 
